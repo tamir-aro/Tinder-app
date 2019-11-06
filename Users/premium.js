9475047840f2d57447dfd5b6aab8edc34c00a17a
nadavzak <57315216+nadavzak@users.noreply.github.com>
2019-11-06 18:12:29 +0200

    Add files via upload

---
+++ Users/premium.js
@@ -0,0 +1,17 @@
+﻿import profile from "./profile";
+
+export default class premium extends profile {
+    constructor(hobbies) {
+        
+        super(id, name, gender, age, height, location, image, premium)
+        this.hobbies = hobbies;
+
+    }
+
+      render() {
+        super(temp);
+        hobbies.map(temp => temp += `<p>${hobbies} </p>`);
+        
+    }
+    
+}
